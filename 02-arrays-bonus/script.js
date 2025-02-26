ee143e289acaedf39612c7022d6b78d8035506c5
GiuseppeDep <giuseppe.deprisco05@gmail.com>
2025-02-26 20:50:27 +0100

    feat:eser completo

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -8,12 +8,49 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+for (let i = 0; i < teachers.length; i++) {
+  let curTeacher = teachers[i];
+  console.log(curTeacher);
+  
+  
+}
+
+
+
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
-// e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+// e salva il risultato nella variabile reversedTeachers 
+for (let i = teachers.length -1 ; i >= 0; i--){
+  const reversedTeachers = teachers[i];
+console.log(reversedTeachers);
+}
+
+
+// const reversedTeachers = []; 
+// for (let i = teachers.length - 1; i >= 0; i--){
+// const curTeacher = teachers[i];
+// reversedTeachers.push(curTeacher);
+// }
+// console.log(reversedTeachers);
+
+
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = [];
+for (let i = 0; i < teachers.length; i++){
+  const curTeacher = teachers[i];
+  if (curTeacher.length >=5){
+    longNames.push(curTeacher);
+  }
+}
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const edIndex = teachers.indexOf("Ed");
+console.log(edIndex);
+teachers.splice(edIndex,1);
+console.log(teachers);
+
+
